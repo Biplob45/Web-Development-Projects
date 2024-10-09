0ab37ebd8c538d48ba582f43d111fa107dcb9453
Md Badiuzzaman Biplob <mdbadiuzzamanbiplob@gmail.com>
2024-10-09 22:54:19 +0600

    Taste Bees - Website for a Restaurant
    Taste Bees - Website for a Restaurant

---
+++ Taste Bees - Website for a Restaurant/tastebees-codes/components/footer.php
@@ -0,0 +1,34 @@
+<footer class="footer">
+
+    <section class="grid">
+
+        <div class="box">
+            <img src="images/email-icon.png" alt="">
+            <h3>Our email</h3>
+            <a href="mailto:leokonika@gmail.com">leokonika@gmail.com</a>
+        </div>
+
+        <div class="box">
+            <img src="images/clock-icon.png" alt="">
+            <h3>Opening hours</h3>
+            <p>00:08am to 10:00pm</p>
+        </div>
+
+        <div class="box">
+            <img src="images/map-icon.png" alt="">
+            <h3>Our address</h3>
+            <a href="#">Agrabad, Chittagong - 4000</a>
+        </div>
+
+        <div class="box">
+            <img src="images/phone-icon.png" alt="">
+            <h3>Our number</h3>
+            <a href="tel:1234567890">0123456789</a>
+        </div>
+
+    </section>
+
+    <div class="credit">&copy; copyright @ <?=date('Y');?> by <span>Mahmuda Samia Konika</span> | all rights reserved!
+    </div>
+
+</footer>
